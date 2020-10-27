8b9caa35b6c64892d31aebfd44abb914638050f8
Kjell Vos <vos.kjell@gmail.com>
2020-10-27 12:55:14 +0100

    Displays colors now too.

---
+++ edit_data.py
@@ -0,0 +1,25 @@
+import csv
+
+from abbreviations import us_state_abbrev
+
+states = {}
+with open('covid-19-dataset-1.csv') as f:
+    reader = csv.reader(f)
+    next(reader)  # skip the first line with the column heads
+
+    for row in reader:
+        state_name = row[2]
+        confirmed = int(row[7])
+
+        if state_name in us_state_abbrev.keys():
+            if state_name in states:
+                states[us_state_abbrev[state_name]] += confirmed
+            else:
+                states[us_state_abbrev[state_name]] = confirmed
+
+        with open('data-edited.csv', mode='w', newline='') as data_edited:
+            state_writer = csv.writer(data_edited, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            state_writer.writerow("StateCode,Confirmed")
+            for state in states:
+                state_writer.writerow([state, states[state]])
--- main.py
+++ main.py
@@ -13,79 +13,30 @@ def index():
     start_coords = (45.5236, -122.6750)
     folium_map = folium.Map(location=start_coords, zoom_start=3)
 
-    states = {}
-    with open('covid-19-dataset-1.csv') as f:
-        reader = csv.reader(f)
-        next(reader)  # skip the first line with the column heads
-
-        for row in reader:
-            state_name = row[2]
-            confirmed = int(row[7])
-
-            if state_name in us_state_abbrev.keys():
-                if state_name in states:
-                    states[us_state_abbrev[state_name]] += confirmed
-                else:
-                    states[us_state_abbrev[state_name]] = confirmed
-
-    data = {
-        'StateCode': states.keys(),
-        'Confirmed': states.values()
-    }
-
+    data = pd.read_csv("data-edited.csv")
+    print(data)
     data_frame = pd.DataFrame(data, columns=['StateCode', 'Confirmed'])
     pd.set_option("display.max_rows", None, "display.max_columns", None)
     print(data_frame)
 
+    bins = [0, 10, 50, 100, 250, 500, 800, 1000]
     folium.Choropleth(
         geo_data="us-states.json",
         name='choropleth',
+        bins=bins,
         data=data_frame,
         columns=['StateCode', 'Confirmed'],
-        key_on='feature.properties.name',
+        key_on='feature.id',
         fill_opacity=0.7,
         line_opacity=0.2,
+        fill_color='YlOrRd',
         legend_name='Infected people'
     ).add_to(folium_map)
 
-    return folium_map._repr_html_()
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-"""
-@app.route('/')
-def index():
-    start_coords = (45.5236, -122.6750)
-    folium_map = folium.Map(location=start_coords, zoom_start=10)
-
-    data = list()
-    first_line = True
-    with open('covid-19-dataset-1.csv') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if first_line:
-                first_line = False
-            else:
-                lat = row[5]
-                long = row[6]
-                count = row[7]
-
-                if len(lat) > 0 and len(long) > 0:
-                    i = 0
-                    while int(count) > i:
-                        lat = float(lat)
-                        long = float(long)
-                        data.append([lat, long])
-                        i += 1
-
-    hm = plugins.HeatMap(data)
-    folium_map.add_child(hm)
+    folium.LayerControl().add_to(folium_map)
 
     return folium_map._repr_html_()
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
