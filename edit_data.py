0543b94a1c748bd1e197f00d268851ccdc32cbf1
Kjell Vos <vos.kjell@gmail.com>
2020-10-27 14:36:04 +0100

    Fixed legend and totally works now

--- edit_data.py
+++ edit_data.py
@@ -12,14 +12,14 @@ with open('covid-19-dataset-1.csv') as f:
         confirmed = int(row[7])
 
         if state_name in us_state_abbrev.keys():
-            if state_name in states:
-                states[us_state_abbrev[state_name]] += confirmed
+            if us_state_abbrev[state_name] in states:
+                states[us_state_abbrev[state_name]] = states[us_state_abbrev[state_name]] + confirmed
             else:
                 states[us_state_abbrev[state_name]] = confirmed
 
-        with open('data-edited.csv', mode='w', newline='') as data_edited:
-            state_writer = csv.writer(data_edited, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    with open('data-edited.csv', mode='w', newline='') as data_edited:
+        state_writer = csv.writer(data_edited, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-            state_writer.writerow(["StateCode", "Confirmed"])
-            for state in states:
-                state_writer.writerow([state, states[state]])
+        state_writer.writerow(["StateCode", "Confirmed"])
+        for state in states:
+            state_writer.writerow([state, states[state]])
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 
+import branca
 import folium
 import pandas as pd
 from abbreviations import us_state_abbrev
@@ -14,14 +15,17 @@ def index():
     folium_map = folium.Map(location=start_coords, zoom_start=3)
 
     data = pd.read_csv("data-edited.csv")
-    print(data)
     data_frame = pd.DataFrame(data, columns=['StateCode', 'Confirmed'])
     pd.set_option("display.max_rows", None, "display.max_columns", None)
     print(data_frame)
 
+    colormap = branca.colormap.linear.YlOrRd_09.scale(0, 105000).to_step(6)
+    colormap.caption = 'Infected people'
+    folium_map.add_child(colormap)
+
     #  bins = [0, 10, 50, 100, 250, 500, 800, 1000]
-    bins = data_frame['Confirmed'].quantile([0, 0.05, 0.1, 0.4, 0.8, 1])
-    folium.Choropleth(
+    bins = data_frame['Confirmed'].quantile([0, 0.16, 0.32, 0.48, 0.64, 0.80, 1])
+    choropleth = folium.Choropleth(
         geo_data="us-states.json",
         name='choropleth',
         bins=bins,
@@ -31,10 +35,12 @@ def index():
         fill_opacity=0.7,
         line_opacity=0.2,
         fill_color='YlOrRd',
-        legend_name='Infected people'
-    ).add_to(folium_map)
+    )
+    for key in choropleth._children:
+        if key.startswith('color_map'):
+            del(choropleth._children[key])
 
-    folium.LayerControl().add_to(folium_map)
+    choropleth.add_to(folium_map)
 
     return folium_map._repr_html_()
 
